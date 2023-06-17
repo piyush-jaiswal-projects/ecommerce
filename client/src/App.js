800fb583ccd18a16687f3de47d49635e7f51eda5
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-18 00:02:39 +0530

    Design Setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { Logo1, Logo2 } from "./constants/images";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-base">
       <img src={Logo1} alt="" />
       <img src={Logo2} alt="" />
       <p className="font-bold">Welcome</p>
---
+++ client/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      base: '#FAF0E4',
+      primary: '#FFE569',
+      secondary: '#F79327'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        base: '#FAF0E4',
+        primary: '#FFE569',
+        secondary: '#F79327'
+      }
+    },
+    plugins: [],
+  }
+}
