7202809fdca2be8f0915d578c9c7419b0f29be7d
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2023-06-20 18:29:13 +0530

    Product Listing Page

--- client/src/components/Navbar/SmallCategories.js
+++ client/src/components/Navbar/SmallCategories.js
@@ -1,9 +1,17 @@
 import NavLink from "../Links/NavLinks"
+import { useDispatch } from "react-redux"
+import { changeCategory } from "../../reducers/productReducer";
 
 export default function SmallCategories(props) {
+    const dispatch = useDispatch();
+
+    function handleCategoryChange(item) {
+        dispatch(changeCategory({categoryName: item}))
+    }
+
     return (
         <div id='CategoriesBar' className="hidden fixed top-1 right-2 z-50 rounded-md text-center bg-primary p-4 mt-14  w-[300px]">
-                {props.links.map((item)=> {return <div className='my-2' key={item + "small"}><NavLink key={item+"small"} text={item} url="/" /></div>})}
+            {props.links.map((item)=> {return <p onClick={()=>handleCategoryChange(item)} className="cursor-pointer mx-4 text-center font-bold hover:text-secondary" key={item+"large"} >{item}</p>})}
         </div>
     )
 }
